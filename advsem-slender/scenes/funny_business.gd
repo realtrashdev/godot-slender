84f1b1f15d281f786133e15990b057ff8d066fd8
Matthew <matthew.oneill@mymail.champlain.edu>
2025-09-03 21:59:33 -0400

    Disgusting Shader of Evil

---
+++ advsem-slender/scenes/funny_business.gd
@@ -0,0 +1,15 @@
+extends Node3D
+
+@export var shader_material: ShaderMaterial
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	#for i in range(4):
+	#	var color: Color = Color(randf(), randf(), randf(), 1)
+	#	shader_material.set_shader_parameter("shade_" + str(i), color)
+	pass
