88e3c2b6b19185cd82f9ff8a87a03296dc03b446
Matthew <matthew.oneill@mymail.champlain.edu>
2025-09-25 16:59:43 -0400

    adjusted menu to in game transition

--- advsem-slender/scripts/player/player_camera.gd
+++ advsem-slender/scripts/player/player_camera.gd
@@ -1,12 +1,6 @@
 extends Camera3D
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var tween = create_tween()
-	tween.tween_property(self, "fov", 80, 2).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	tween.tween_property(self, "fov", 80, 1.0).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
