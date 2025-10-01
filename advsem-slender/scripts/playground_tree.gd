ae33643a7337a80a661e9729ac7a510f746ea6ac
Matthew O'Neill <matthew.oneill@mymail.champlain.edu>
2025-10-01 19:57:48 -0400

    Revert "Auto stash before merge of "main" and "origin/main""
    This reverts commit 533076b289dd407ec2dc8227408084ed1b3c9955.

--- advsem-slender/scripts/playground_tree.gd
+++ advsem-slender/scripts/playground_tree.gd
@@ -1,11 +1,6 @@
 extends Node3D
 
-const MIN_SCALE = 0.9
-const MAX_SCALE = 1.3
 
 func _ready() -> void:
-	var rand = randf_range(MIN_SCALE, MAX_SCALE)
+	var rand = randf_range(0.8, 1.3)
 	scale = Vector3(rand, rand, rand)
-	
-	rand = randf_range(0, 360)
-	rotation.y = deg_to_rad(rand)
